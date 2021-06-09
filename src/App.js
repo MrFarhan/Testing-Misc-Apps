f3732f6744180db4bf8aa1d6f334583b86573a0b
MrFarhan <muhammadfarhan012345@gmail.com>
2021-06-09 13:19:01 +0500

    removed extra code / created basic app

--- src/App.js
+++ src/App.js
@@ -1,62 +1,12 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
 import './App.css';
-import { useTranslation, Trans } from 'react-i18next';
-import { useState, Suspense, useEffect } from 'react';
-import Footer from './Footer'
 
-function App() {
-  const { t, i18n } = useTranslation();
-  const [count, setCounter] = useState(0);
-
-  const [lngs, setLngs] = useState({ en: { nativeName: 'English' } });
-
-  useEffect(() => {
-    i18n.services.backendConnector.backend.getLanguages((err, ret) => {
-      if (err) return // TODO: handle err...
-      console.log("err is ", err, "ret is ", ret)
-      setLngs(ret);
-    });
-  }, [i18n.services.backendConnector.backend]);
+export default function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          {Object.keys(lngs).map((lng) => (
-            <button key={lng} style={{ fontWeight: i18n.language === lng ? 'bold' : 'normal' }} type="submit" onClick={() => {
-              i18n.changeLanguage(lng);
-              setCounter(count + 1);
-            }}>
-              {lngs[lng].nativeName}
-            </button>
-          ))}
-        </div>
-        <p>
-          <i>{t('dummyKey')}</i>
-        </p>
-        <p>
-          <Trans i18nKey="welcomeKey">
-            Edit <code>src/App.js</code> and save to reload.
-          </Trans>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {t('dummyKey')}
-        </a>
-      </header>
-      <Footer t={t} />
+      Hello world
     </div>
-  );
-}
-
-// here app catches the suspense from page in case translations are not yet loaded
-export default function WrappedApp() {
-  return (
-    <Suspense fallback="...is loading">
-      <App />
-    </Suspense>
   );
 }
